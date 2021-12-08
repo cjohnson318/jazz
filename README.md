7b573638ec1c77c8f629ebe729004d94cc736019
Connor Johnson <connor.labaume.johnson@gmail.com>
2021-12-07 20:14:47 -0700

    Add README and requirements

---
+++ README.md
@@ -0,0 +1,12 @@
+# Jazz
+
+Scripts to read chords from a file, and then generate MIDI data to be played by
+a MIDI device.
+
+## Usage
+
+By default, this plays Blue in Green, but you can supply a file to be played.
+
+```bash
+python3 app.py blue-in-green.txt
+```
--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import sys
+
 import parse
 import utils
 
 if __name__ == '__main__':
     device = utils.init()
 
-    filename = 'autumn-leaves.txt'
-    filename = 'blue-in-green.txt'
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        filename = 'blue-in-green.txt'
     score = parse.lead_sheet(filename)
     
     utils.quit_listener()
